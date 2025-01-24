285c415c43c82f62232864cf08eed72f2e603055
divyamjangra <divyam0137.becse24@chitkara.edu.in>
2025-01-24 09:21:18 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -3,10 +3,10 @@
 int main() {
     int a, b;
 
-    // Input two space-separated integers
+
     scanf("%d %d", &a, &b);
 
-    // Check if the numbers are not equal
+
     if (a >0 || b>0)
         printf("True");
     else
