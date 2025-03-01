c3234f294e4150a5a9b8b4051e28bedb52f61256
divyamjangra <divyam0137.becse24@chitkara.edu.in>
2025-03-01 14:25:42 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -9,15 +9,23 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    int max_right = arr[N - 1]; // The last element is always a leader
-    printf("%d ", max_right);
+    int leaders[N];  // Array to store leaders
+    int leader_count = 0;
     
-    // Iterate from second last element to the first element
+    int max_right = arr[N - 1];  // The last element is always a leader
+    leaders[leader_count++] = max_right;
+    
+    // Traverse the array from right to left to find leaders
     for (int i = N - 2; i >= 0; i--) {
         if (arr[i] >= max_right) {
-            printf("%d ", arr[i]);
-            max_right = arr[i]; // Update the max_right
+            leaders[leader_count++] = arr[i];
+            max_right = arr[i];  // Update the max_right
         }
+    }
+
+    // Print the leaders in the correct order (from left to right)
+    for (int i = leader_count - 1; i >= 0; i--) {
+        printf("%d ", leaders[i]);
     }
     
     return 0;
