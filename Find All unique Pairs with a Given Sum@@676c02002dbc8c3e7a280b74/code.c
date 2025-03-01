04c3d808da56f55d4478236aed516b3a055c600b
divyamjangra <divyam0137.becse24@chitkara.edu.in>
2025-03-01 14:19:05 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,46 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void findUniquePairs(int arr[], int n, int target) {
-    qsort(arr, n, sizeof(int), (int (*)(const void*, const void*))cmp);
-
-    int left = 0, right = n - 1;
+void findPairs(int arr[], int n, int target) {
+    // Sort the array to make it easier to find pairs
+    qsort(arr, n, sizeof(int), (int(*)(const void*, const void*))compare);
+    
+    int left = 0;
+    int right = n - 1;
 
     while (left < right) {
         int sum = arr[left] + arr[right];
-        
+
         if (sum == target) {
             printf("%d %d\n", arr[left], arr[right]);
-            while (left < right && arr[left] == arr[left + 1]) left++;
-            while (left < right && arr[right] == arr[right - 1]) right--;
             left++;
             right--;
-        } else if (sum < target) {
+            
+            // Skip duplicates
+            while (left < right && arr[left] == arr[left - 1]) left++;
+            while (left < right && arr[right] == arr[right + 1]) right--;
+        } 
+        else if (sum < target) {
             left++;
-        } else {
+        } 
+        else {
             right--;
         }
     }
 }
 
-int cmp(const int *a, const int *b) {
-    return (*a - *b);
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
 }
 
 int main() {
-    int n, target;
+    int N, T;
+
+    // Read input
+    scanf("%d", &N);
+    int arr[N];
     
-    scanf("%d", &n);
-    
-    int arr[n];
-    
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < N; i++) {
         scanf("%d", &arr[i]);
     }
-    
-    scanf("%d", &target);
-    
-    findUniquePairs(arr, n, target);
+
+    scanf("%d", &T);
+
+    // Find and print pairs
+    findPairs(arr, N, T);
 
     return 0;
 }
+
