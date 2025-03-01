8003ae7c0521aaee42fb8b06b524b30302039595
divyamjangra <divyam0137.becse24@chitkara.edu.in>
2025-03-01 14:22:10 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Declaration of the compare function
-int compare(const void *a, const void *b);
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
 
 void findPairs(int arr[], int n, int target) {
-    // Sort the array to make it easier to find pairs
     qsort(arr, n, sizeof(int), compare);
-    
+
     int left = 0;
     int right = n - 1;
 
@@ -15,49 +15,31 @@ void findPairs(int arr[], int n, int target) {
         int sum = arr[left] + arr[right];
 
         if (sum == target) {
-            // Print the pair in ascending order
-            if (arr[left] < arr[right]) {
-                printf("%d %d\n", arr[left], arr[right]);
-            } else {
-                printf("%d %d\n", arr[right], arr[left]);
-            }
+            printf("%d %d\n", arr[left], arr[right]);
             left++;
             right--;
-            
-            // Skip duplicates
             while (left < right && arr[left] == arr[left - 1]) left++;
             while (left < right && arr[right] == arr[right + 1]) right--;
-        } 
-        else if (sum < target) {
+        } else if (sum < target) {
             left++;
-        } 
-        else {
+        } else {
             right--;
         }
     }
 }
 
-// Compare function for qsort
-int compare(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
-}
-
 int main() {
     int N, T;
-
-    // Read input
     scanf("%d", &N);
     int arr[N];
-    
+
     for (int i = 0; i < N; i++) {
         scanf("%d", &arr[i]);
     }
 
     scanf("%d", &T);
 
-    // Find and print pairs
     findPairs(arr, N, T);
 
     return 0;
 }
-
