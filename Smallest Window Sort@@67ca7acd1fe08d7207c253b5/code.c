b8d92c9bbc79d2ee6ffc922ec6356cd56ceaf1fc
divyamjangra <divyam0137.becse24@chitkara.edu.in>
2025-03-08 13:04:10 +0530

    Solution to Smallest Window Sort

---
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int findMinSortedSubarray(int arr[], int n) {
+    int* temp = (int*) malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        temp[i] = arr[i];
+    }
+
+    // Sort the temporary array
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (temp[i] > temp[j]) {
+                int t = temp[i];
+                temp[i] = temp[j];
+                temp[j] = t;
+            }
+        }
+    }
+
+    int start = -1, end = -1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != temp[i]) {
+            if (start == -1) {
+                start = i;
+            }
+            end = i;
+        }
+    }
+
+    if (start == -1) {
+        return 0;
+    }
+
+    return end - start + 1;
+}
+
