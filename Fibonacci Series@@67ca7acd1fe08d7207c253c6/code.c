1998fbafff7fa3a58edbe02c5bf0aedf5fc2708a
divyamjangra <divyam0137.becse24@chitkara.edu.in>
2025-03-08 12:16:35 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -17,17 +17,3 @@ void fibonacciSeries(int n) {
     }
     printf("\n");
 }
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    
-    if (n <= 0) {
-        printf("Invalid input\n");
-        return 0;
-    }
-    
-    fibonacciSeries(n);
-    
-    return 0;
-}
