8f8d159dace815ce4e72061df85f80ba0558a632
divyamjangra <divyam0137.becse24@chitkara.edu.in>
2025-03-11 10:09:02 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,65 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int comparePairs(const void *a, const void *b) {
-    int *pairA = (int *)a;
-    int *pairB = (int *)b;
-    if (pairA[0] != pairB[0]) {
-        return pairA[0] - pairB[0];
-    } else {
-        return pairA[1] - pairB[1];
-    }
-}
-
-void findPairs(int arr[], int n, int target) {
-    qsort(arr, n, sizeof(int), comparePairs); // Use comparePairs to sort the array
-
+void findUniquePairs(int arr[], int n, int target) {
+    qsort(arr, n, sizeof(int), compare);
     int left = 0;
     int right = n - 1;
-
-    int pairs[n][2]; // To store the pairs
-    int pairCount = 0;
-
-    // Find all pairs and store them
+    
     while (left < right) {
         int sum = arr[left] + arr[right];
-
+        
         if (sum == target) {
-            pairs[pairCount][0] = arr[left];
-            pairs[pairCount][1] = arr[right];
-            pairCount++;
+            printf("%d %d\n", arr[left], arr[right]);
             left++;
             right--;
-            while (left < right && arr[left] == arr[left - 1]) left++;  // Skip duplicates
-            while (left < right && arr[right] == arr[right + 1]) right--;  // Skip duplicates
-        } else if (sum < target) {
+            while (left < right && arr[left] == arr[left - 1]) left++;
+            while (left < right && arr[right] == arr[right + 1]) right--;
+        }
+        else if (sum < target) {
             left++;
-        } else {
+        }
+        else {
             right--;
         }
     }
+}
 
-    // Sort the pairs based on both elements of each pair
-    qsort(pairs, pairCount, sizeof(pairs[0]), comparePairs);
-
-    // Print the pairs
-    for (int i = 0; i < pairCount; i++) {
-        printf("%d %d\n", pairs[i][0], pairs[i][1]);
-    }
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
 }
 
 int main() {
-    int N, T;
-    scanf("%d", &N);
-    int arr[N];
-
-    for (int i = 0; i < N; i++) {
+    int n, target;
+    scanf("%d", &n);
+    int arr[n];
+    
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
-    scanf("%d", &T);
-
-    findPairs(arr, N, T);
-
+    
+    scanf("%d", &target);
+    findUniquePairs(arr, n, target);
+    
     return 0;
 }
