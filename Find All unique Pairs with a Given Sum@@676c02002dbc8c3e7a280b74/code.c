90df404bfd7627cddf7a1ac5176ce4a099500431
divyamjangra <divyam0137.becse24@chitkara.edu.in>
2025-03-17 16:38:34 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,45 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void findUniquePairs(int arr[], int n, int target) {
-    qsort(arr, n, sizeof(int), compare);
-    int left = 0;
-    int right = n - 1;
-    
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        
-        if (sum == target) {
-            printf("%d %d\n", arr[left], arr[right]);
-            left++;
-            right--;
-            while (left < right && arr[left] == arr[left - 1]) left++;
-            while (left < right && arr[right] == arr[right + 1]) right--;
-        }
-        else if (sum < target) {
-            left++;
-        }
-        else {
-            right--;
+void findPairs(int arr[], int n, int target) {
+    qsort(arr, n, sizeof(int), (int (*)(const void*, const void*))strcmp);
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target) {
+                printf("%d %d\n", arr[i], arr[j]);
+                break;
+            }
         }
     }
-}
-
-int compare(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
 }
 
 int main() {
     int n, target;
+
     scanf("%d", &n);
     int arr[n];
-    
+
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    
+
     scanf("%d", &target);
-    findUniquePairs(arr, n, target);
-    
+
+    findPairs(arr, n, target);
+
     return 0;
 }
